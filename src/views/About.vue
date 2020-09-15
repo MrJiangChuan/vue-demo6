36cd57dcec62caf86203649c13f75da2c481c6fb
jiangchuan <jiangchuan_1994@163.com>
2020-09-15 23:22:30 +0800

    state使用【两种方式】

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>1、state使用【两种方式】</h1>
+    <h2>store.count={{$store.state.count}}</h2>
+    <h2>mapstate.count={{count}}</h2>
   </div>
 </template>
+
+<script>
+  import {mapState} from 'vuex'
+  export default {
+    name: 'About',
+    data() {
+      return {
+
+      }
+    },
+    computed: {
+      ...mapState(['count'])
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // publicPath: './',
+  // outputDir: 'dist',
+  // assetsDir: 'static'
+  devServer: {
+    port: 80,
+    open: true
+  }
+  // runtimeCompiler: true
+}
