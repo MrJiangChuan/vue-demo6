3162195728098d607ba4eecf093299e6e5f6657f
jiangchuan <jiangchuan_1994@163.com>
2020-09-15 23:48:45 +0800

    mutations使用【两种方式】

--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,14 @@ export default new Vuex.Store({
     count: 10
   },
   mutations: {
+    //不带参数
+    add(state){
+      state.count++
+    },
+    //带参数
+    addN(state, payload){
+      state.count += payload
+    }
   },
   actions: {
   },
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,11 +3,16 @@
     <h1>1、state使用【两种方式】</h1>
     <h2>store.count={{$store.state.count}}</h2>
     <h2>mapstate.count={{count}}</h2>
+    <h1>2、mutations使用【两种方式】</h1>
+    <input type="button" value="mutations使用[不带参]" @click="handler1"/>
+    <input type="button" value="mutations使用[带参]" @click="handler2"/><br>
+    <input type="button" value="mapMutations使用[不带参]" @click="add"/>
+    <input type="button" value="mapMutations使用[带参]" @click="addN(5)"/>
   </div>
 </template>
 
 <script>
-  import {mapState} from 'vuex'
+  import {mapState,mapMutations} from 'vuex'
   export default {
     name: 'About',
     data() {
@@ -17,6 +22,15 @@
     },
     computed: {
       ...mapState(['count'])
+    },
+    methods: {
+      handler1() {
+          this.$store.commit('add')
+      },
+      handler2() {
+        this.$store.commit('addN',10)
+      },
+      ...mapMutations(['add','addN'])
     }
   }
 </script>
